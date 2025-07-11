1bc3fc1744c706f0b456aa22cd29d46883697a4c
kvskfoods <foodskvsk@gmail.com>
2025-07-11 23:20:28 +0530

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "@strapi/cloudinary",
+      provider: "cloudinary",
       providerOptions: {
         cloud_name: env("CLOUDINARY_NAME"),
         api_key: env("CLOUDINARY_KEY"),
